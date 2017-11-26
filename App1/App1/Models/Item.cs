17f7e325eda01278e198133fba4d03c27983bdd7
Depechie <glenn.versweyveld@gmail.com>
2017-11-26 23:04:49 +0100

    Initial checkin

---
+++ App1/App1/Models/Item.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+
+namespace App1
+{
+    public class Grouping<K, T> : ObservableCollection<T>
+    {
+        public K Key { get; private set; }
+        public Grouping(K key, IEnumerable<T> items)
+        {
+            Key = key;
+            foreach (var item in items)
+                this.Items.Add(item);
+        }
+    }
+
+    public class Item
+    {
+        public string Id { get; set; }
+        public string Text { get; set; }
+        public string Description { get; set; }
+    }
+}
---
+++ App1/App1/ViewModels/ItemsViewModel.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+
+namespace App1
+{
+    public class ItemsViewModel : BaseViewModel
+    {
+        public ObservableCollection<Grouping<string, Item>> items;
+        public ObservableCollection<Grouping<string, Item>> Items
+        {
+            get { return items; }
+            set { SetProperty(ref items, value); }
+        }
+        public Command LoadItemsCommand { get; set; }
+
+        public ItemsViewModel()
+        {
+            Title = "Browse";
+            Items = new ObservableCollection<Grouping<string, Item>>();
+            LoadItemsCommand = new Command(async () => await ExecuteLoadItemsCommand());
+
+            MessagingCenter.Subscribe<NewItemPage, Item>(this, "AddItem", async (obj, item) =>
+            {
+                var _item = item as Item;
+                //Items.Add(_item);
+                await DataStore.AddItemAsync(_item);
+            });
+        }
+
+        async Task ExecuteLoadItemsCommand()
+        {
+            if (IsBusy)
+                return;
+
+            IsBusy = true;
+
+            try
+            {
+                Items.Clear();
+                var items = await DataStore.GetItemsAsync(true);
+
+                var groupedItems = from item in items
+                                   orderby item.Text
+                                   group item by item.Text[0].ToString() into grouped
+                                   select new Grouping<string, Item>(grouped.Key, grouped);
+
+                Items = new ObservableCollection<Grouping<string, Item>>(groupedItems);
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(ex);
+            }
+            finally
+            {
+                IsBusy = false;
+            }
+        }
+    }
+}
